7f5079eaf47c84c0deb7b6e85406ed75c4946b7e
MoonsuKang <rkdms4971@naver.com>
2025-04-01 20:49:28 +0900

    add: implement initial unit tests using Kotest

---
+++ app/src/test/java/com/river/kotest/MyTests.kt
@@ -0,0 +1,15 @@
+package com.river.kotest
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.startWith
+
+class MyTests : StringSpec({
+    "length should return size of string" {
+        "hello".length shouldBe 5 // 실제로 "hello"의 length가 5 인가요?
+    }
+    "startsWith should test for a prefix" {
+        "world" should startWith("wor") // 실제로 "world"가 "wor"로 시작하나요?
+    }
+})
