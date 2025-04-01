34d04684c16ab89b09dcd55978b62d419b3ff8a5
MoonsuKang <rkdms4971@naver.com>
2025-04-02 00:40:08 +0900

    add: implement nested tests using DescribeSpec in Kotest

---
+++ app/src/test/java/com/river/kotest/NestedTest.kt
@@ -0,0 +1,15 @@
+package com.river.kotest
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class NestedTest : DescribeSpec({
+    describe("an outer test") { // <- 컨테이너
+        it("an inner test") { //<- 케이스
+            1 + 2 shouldBe 3
+        }
+        it("an inner test too!") { // <- 케이스
+            3 + 4 shouldBe 7
+        }
+    }
+})
