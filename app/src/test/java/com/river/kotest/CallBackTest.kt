2cbcc214fc4e3ede534128e4910175c30743d7f9
MoonsuKang <rkdms4971@naver.com>
2025-04-02 00:40:16 +0900

    add: implement callback tests using FunSpec in Kotest

---
+++ app/src/test/java/com/river/kotest/CallBackTest.kt
@@ -0,0 +1,20 @@
+package com.river.kotest
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.string.shouldHaveLength
+
+class CallBackTest : FunSpec({
+
+    beforeEach {
+        println("Hello from $it")
+    }
+
+    test("sam should be a three letter name") {
+        "sam".shouldHaveLength(3)
+    }
+
+    afterEach {
+        println("GoodBye from $it")
+    }
+
+})
---
+++ app/src/test/java/com/river/kotest/ReusableCallbacks.kt
@@ -0,0 +1,17 @@
+package com.river.kotest
+
+import io.kotest.core.spec.BeforeTest
+import io.kotest.core.spec.style.FunSpec
+
+val resetDatabase: BeforeTest = {
+    println("DB 초기화")
+}
+
+class ReusableCallbacks : FunSpec({
+
+    beforeTest(resetDatabase)
+
+    test("DB가 깨끗해야하는 테스트") {
+        println("테스트 실행")
+    }
+})
